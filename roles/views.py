f2a1529a400e3c6a176766e059babe182dfe8ded
roeurtke <kesey.roeurth@gmail.com>
2025-03-22 00:57:58 +0700

    Updated roles/views.py file.

--- roles/views.py
+++ roles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from .models import Role
 from .serializers import RoleSerializer
 from permissions.permissions import HasPermission
@@ -11,6 +12,10 @@ class RoleListCreateView(generics.ListCreateAPIView):
         if self.request.method == 'GET':
             return [permissions.IsAuthenticated(), HasPermission('can_view_list_role')]
         return [permissions.IsAuthenticated(), HasPermission('can_create_role')]
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({"message": "Role created successfully", "user": response.data}, status=status.HTTP_201_CREATED)
 
 class RoleRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Role.objects.all()
@@ -24,3 +29,12 @@ class RoleRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         elif self.request.method == 'DELETE':
             return [permissions.IsAuthenticated(), HasPermission('can_delete_role')]
         return [permissions.IsAuthenticated()]
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return Response({"message": "Role updated successfully", "user": response.data}, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "The Role is deleted."}, status=status.HTTP_200_OK)

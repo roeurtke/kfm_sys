8de1726e7cb433a507f9cbcba8793fd36f74e5cf
kroeurt <kesei.roeurt@allweb.com.kh>
2025-05-16 16:43:10 +0700

    Added authorized for RolePermission module.

--- permissions/views.py
+++ permissions/views.py
@@ -48,9 +48,39 @@ class PermissionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView)
 class RolePermissionListCreateView(generics.ListCreateAPIView):
     queryset = RolePermission.objects.all()
     serializer_class = RolePermissionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.IsAuthenticated(), HasPermission('can_view_list_role_permission')]
+        return [permissions.IsAuthenticated(), HasPermission('can_create_role_permission')]
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({"message": "Role and Permission created successfully", "role_permission": response.data}, status=status.HTTP_201_CREATED)
 
 class RolePermissionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = RolePermission.objects.all()
     serializer_class = RolePermissionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.IsAuthenticated(), HasPermission('can_view_role_permission')]
+        elif self.request.method in ['PUT', 'PATCH']:
+            return [permissions.IsAuthenticated(), HasPermission('can_update_role_permission')]
+        elif self.request.method == 'DELETE':
+            return [permissions.IsAuthenticated(), HasPermission('can_delete_role_permission')]
+        return [permissions.IsAuthenticated()]
+    
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return Response({"message": "Role and Permission updated successfully.", "role_permission": response.data}, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.deleted_at = timezone.now()
+            instance.status = False
+            instance.save()
+            return Response({"message": "Role and Permission deleted successfully."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

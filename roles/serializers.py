3d7798bc0746601505d48bef34bb099a186ed027
roeurtke <kesey.roeurth@gmail.com>
2025-05-18 20:48:06 +0700

    Update role with status.

--- roles/serializers.py
+++ roles/serializers.py
@@ -4,12 +4,19 @@ from .models import Role
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
-        fields = ('id', 'name', 'description')
+        fields = ('id', 'name', 'description', 'status', 'created_at', 'updated_at')
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.context.get('request').method in ['PUT', 'PATCH']:
             self.fields['name'].required = False
+    
+    def updte(self, instance, validated_data):
+        if 'status' in validated_data:
+            instance.status = validated_data['status']
+            
+        instance.save()
+        return instance
     
     def to_representation(self, instance):
         return {
